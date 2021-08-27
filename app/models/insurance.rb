92e90a8573b19d4fc1e8fd82cea64d16327023b2
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 19:54:22 +0800

    app: model: insurance: add has_many logs
    app: model: investment: add has_many logs
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- app/models/insurance.rb
+++ app/models/insurance.rb
@@ -1,2 +1,4 @@
 class Insurance < ApplicationRecord
+    belongs_to :users
+    has_many :logs, as: :loggable
 end
--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,2 +1,4 @@
 class Investment < ApplicationRecord
+    belongs_to :users
+    has_many :logs, as: :loggable
 end
