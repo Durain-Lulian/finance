dbfb1c316440c1043f67f87822baefea4c77806b
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 20:37:17 +0800

    Seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create()
+insurance_provider = Provider.create()
+investmemt_provider = Provider.create()
+
+insurance = Insurance.create(provider: insurance_provider)
+puts insurance.errors.full_messages
+investment = Investment.create(provider: investmemt_provider)
+
+log = Log.create(loggable: insurance)
+puts log.errors.full_messages
+Log.create(loggable: investment)
