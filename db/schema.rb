de03c00feea3f343a5d6500819a340b20495b80f
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 17:47:10 +0800

    db: migrate: add User model
    db: schema: migrate db
    
    others: auto-generated
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_094327) do
+ActiveRecord::Schema.define(version: 2021_08_27_094638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2021_08_27_094327) do
   create_table "providers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
