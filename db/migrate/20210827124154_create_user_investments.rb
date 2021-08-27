f50ac924124e0e595cf189128fd9fd06a34e589c
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 20:43:15 +0800

    Update migration file to add manytomany rs

--- db/migrate/20210827124154_create_user_investments.rb
+++ db/migrate/20210827124154_create_user_investments.rb
@@ -1,8 +1,12 @@
 class CreateUserInvestments < ActiveRecord::Migration[6.1]
   def change
     create_table :user_investments do |t|
-
+      t.uuid :user_id
+      t.uuid :investment_id
       t.timestamps
     end
+
+    add_foreign_key :user_investments, :users
+    add_foreign_key :user_investments, :investments
   end
 end
