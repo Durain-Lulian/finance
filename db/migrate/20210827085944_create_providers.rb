31c674463b43084761395b5df50735dc364e151d
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 17:05:50 +0800

    db: migrate: Add providers attribute
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- db/migrate/20210827085944_create_providers.rb
+++ db/migrate/20210827085944_create_providers.rb
@@ -1,7 +1,8 @@
 class CreateProviders < ActiveRecord::Migration[6.1]
   def change
-    create_table :providers do |t|
-
+    create_table :providers, id: :uuid do |t|
+      t.string :name
+      t.text :description
       t.timestamps
     end
   end
