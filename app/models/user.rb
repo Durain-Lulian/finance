f3ad60b653ece48f02a0c9b3cb8900828b3854cb
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 20:59:50 +0800

    Add m2m rs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :insurances, through: :user_insurances
+    has_many :investments, through: :user_investments
+
+    has_many :user_insurances
+    has_many :user_investments
 end
