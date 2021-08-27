54d31290e7a7d6baa422e47e3db22df3f778ac6a
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 20:41:10 +0800

    Add uuid to insurances

--- db/migrate/20210827093858_create_insurances.rb
+++ db/migrate/20210827093858_create_insurances.rb
@@ -1,6 +1,6 @@
 class CreateInsurances < ActiveRecord::Migration[6.1]
   def change
-    create_table :insurances do |t|
+    create_table :insurances, id: :uuid  do |t|
       t.string :name
       t.text :description
       t.uuid :provider_id
