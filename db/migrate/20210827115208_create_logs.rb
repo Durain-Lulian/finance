af51266573b3d8e594d922b9f39b9e1237068a3b
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 20:36:41 +0800

    Add loggable_type in logs

--- db/migrate/20210827115208_create_logs.rb
+++ db/migrate/20210827115208_create_logs.rb
@@ -2,6 +2,7 @@ class CreateLogs < ActiveRecord::Migration[6.1]
   def change
     create_table :logs, id: :uuid do |t|
       t.uuid :loggable_id
+      t.string :loggable_type
       t.timestamps
     end
   end
