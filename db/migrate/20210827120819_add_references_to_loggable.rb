27895c90df86246ddb20e1a34d5c75b4c525d0ce
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-27 21:00:09 +0800

    Add refereneces to loggable

--- db/migrate/20210827120819_add_references_to_loggable.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToLoggable < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :insurances, :loggable, polymorphic: true, type: :uuid
-    add_reference :investments, :loggable, polymorphic: true, type: :uuid 
-  end
-end
