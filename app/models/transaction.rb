2535a1fc82711a040643db2d7fb20516c065aacd
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-28 18:48:25 +0800

    Add update_logs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,7 +5,31 @@ class Transaction < ApplicationRecord
         cashback_amount = self.cashback_amount
         user = self.user
 
-        insurance = user.user_insurance
+        insurance = user.user_insurance.first
+        investment = user.user_investment.first
+
         insurance_value = insurance.value
+        investment_value = investment.value
+
+        # change
+        next_tier_value = 50
+        if insurance_value < next_tier_value
+            # calculate value needed for next tier
+            insurance_value_to_upgrade = next_tier_value - insurance_value
+
+            # add the value
+            if cashback_amount < insurance_value_to_upgrade
+                insurance_value += cashback_amount
+                cashback_amount = 0
+            else
+                cashback_amount -= insurance_value_to_upgrade
+                insurance_value += insurance_value_to_upgrade
+            end
+        end
+        
+        investment_value += cashback_amount
+        
+        insurance.update(value: insurance_value)
+        investment.update(value: investment_value)
     end
 end
