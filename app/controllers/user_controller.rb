6c81a655c6aeb8d35fe8942b220ca1322354e95c
Bing Yu <bingyu.yap.21@gmail.com>
2021-08-28 18:07:42 +0800

    Add status for return

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,7 @@ class UserController < ApplicationController
     def add_user
         user_id = params[:user_id]
         
-        render json: { user_id: user_id}
+        render json: { user_id: user_id }, status: 200
     end
 end
+    
